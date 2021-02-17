0c2217ccbf15e72788bafa3ee45d8195e3205f8b
Lushpayka <lushpayka98@gmail.com>
2021-02-17 12:05:06 +0300

    first

---
+++ build/js/main.js
@@ -0,0 +1,19 @@
+let acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    for (let j = 0; j < acc.length; j++){
+        if(acc[j] != this){
+          acc[j].classList.remove("active");
+          acc[j].nextElementSibling.style.maxHeight = null;
+      }
+    }
+    let panel = this.nextElementSibling; 
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
